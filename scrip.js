ed3934e0190127f79c7d082bcf1bce882845dc02
SuperDanUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2023-08-22 15:43:47 +0200

    Now you are able to play the complete game.
    Been able to play the best of 7.

--- scrip.js
+++ scrip.js
@@ -1,5 +1,4 @@
 const NUM_OPTIONS = 3;
-
 const OPTIONS = {
   0: "rock",
   1: "paper",
@@ -8,6 +7,12 @@ const OPTIONS = {
   "paper": 1,
   "scissor": 2
 };
+const POINTS_WINNER = 4;
+const PLAYER_1_WIN_ROUND = "You win!";
+const PLAYER_2_WIN_ROUND = "You loose!";
+const DRAW_ROUND = "Draw!";
+const PLAYER_1_WINNER = "You win the game!";
+const PLAYER_2_WINNER = "You loose the game!";
 
 function getBotSelection() {
   return OPTIONS[Math.floor(Math.random() * NUM_OPTIONS)];
@@ -55,4 +60,43 @@ function playRound(playerSelection, botSelection) {
   return result;  
 }
 
-console.log(playRound(getUserSelection(), getBotSelection()));
+function showScoreboard(playerPoints, botPoints) {
+  console.log(`---Scoreboard---
+  Player: ${playerPoints}
+  Bot: ${botPoints}`);
+}
+
+function startGame() {
+  alert("Wellcome to the Rock, Paper, Scissor Game");
+  let botPoints = 0;
+  let playerPoints = 0;
+  let winner = -1;
+  while (winner == -1) {
+    showScoreboard(playerPoints, botPoints);
+    let result = playRound(getUserSelection(), getBotSelection());
+    if (result["winner"] == -1) {
+      result["message"] = DRAW_ROUND + " " + result["message"]; 
+    } else if (result["winner"] == 1) {
+      botPoints++;
+      result["message"] = PLAYER_2_WIN_ROUND + " " + result["message"];
+      if (botPoints == 4) {
+        winner = 1;
+      }
+    } else {
+      playerPoints++;
+      result["message"] = PLAYER_1_WIN_ROUND + " " + result["message"];
+      if (playerPoints == 4) {
+        winner = 0;
+      }
+    }
+    console.log(result["message"]);
+  }
+  if (winner == 0) {
+    console.log(PLAYER_1_WINNER);
+  }
+  else {
+    console.log(PLAYER_2_WINNER);
+  }
+}
+
+startGame();
