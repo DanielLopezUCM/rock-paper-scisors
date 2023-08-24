4e64b9cc3b7a096604dea82419fd1d0af1a30f12
SuperDanUCM <117196575+DanielLopezUCM@users.noreply.github.com>
2023-08-24 12:57:46 +0200

    UI done and funtional

--- scrip.js
+++ scrip.js
@@ -39,64 +39,107 @@ function getUserSelection() {
 }
 
 function playRound(playerSelection, botSelection) {
-  let result = {
-    "winner": -1,
-    "message": ""
-  };
-
+  let message = "";
   if ((OPTIONS[playerSelection] + 1) % 3 == OPTIONS[botSelection]) {
-    result["winner"] = 1;
-    result["message"] = `${botSelection.charAt(0).toUpperCase() + botSelection.substring(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)}`;
+
+    const points = document.querySelector("#botPoints > .points");
+    let botPoints = parseInt(points.textContent);
+    botPoints++;
+    points.textContent = botPoints;
+
+    if (botPoints >= POINTS_WINNER) {
+      message = PLAYER_2_WINNER;
+      endGame();
+    }
+    else {
+      message = PLAYER_2_WIN_ROUND + `${botSelection.charAt(0).toUpperCase() + botSelection.substring(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)}`;
+    }
   }
   else if ((OPTIONS[botSelection] + 1) % 3 == OPTIONS[playerSelection]) {
-    result["winner"] = 0;
-    result["message"] = `${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)} beats ${botSelection.charAt(0).toUpperCase() + botSelection.substring(1)}`;
-  }
-  else {
-    result["winner"] = -1;
-    result["message"] = `Both where ${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)}`
-  }
+    
+    const points = document.querySelector("#playerPoints > .points");
+    let playerPoints = parseInt(points.textContent);
+    playerPoints++;
+    points.textContent = playerPoints;
 
-  return result;  
-}
-
-function showScoreboard(playerPoints, botPoints) {
-  console.log(`---Scoreboard---
-  Player: ${playerPoints}
-  Bot: ${botPoints}`);
-}
-
-function startGame() {
-  alert("Wellcome to the Rock, Paper, Scissor Game");
-  let botPoints = 0;
-  let playerPoints = 0;
-  let winner = -1;
-  while (winner == -1) {
-    showScoreboard(playerPoints, botPoints);
-    let result = playRound(getUserSelection(), getBotSelection());
-    if (result["winner"] == -1) {
-      result["message"] = DRAW_ROUND + " " + result["message"]; 
-    } else if (result["winner"] == 1) {
-      botPoints++;
-      result["message"] = PLAYER_2_WIN_ROUND + " " + result["message"];
-      if (botPoints == 4) {
-        winner = 1;
-      }
-    } else {
-      playerPoints++;
-      result["message"] = PLAYER_1_WIN_ROUND + " " + result["message"];
-      if (playerPoints == 4) {
-        winner = 0;
-      }
+    if (playerPoints >= POINTS_WINNER) {
+      message = PLAYER_1_WINNER;
+      endGame();
     }
-    console.log(result["message"]);
-  }
-  if (winner == 0) {
-    console.log(PLAYER_1_WINNER);
+    else {
+      message = PLAYER_1_WIN_ROUND + `${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)} beats ${botSelection.charAt(0).toUpperCase() + botSelection.substring(1)}`;
+    }
   }
   else {
-    console.log(PLAYER_2_WINNER);
+    message = DRAW_ROUND + `Both where ${playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1)}`
   }
+  const result = document.querySelector("#result");
+  result.textContent = message;
 }
 
-startGame();
+function enableButtons() {
+  const buttons = document.querySelectorAll("#buttons > button");
+  
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+}
+
+function disableButtons() {
+  const buttons = document.querySelectorAll("#buttons > button");
+  
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+}
+
+function setUpButtons() {
+  const buttons = document.querySelectorAll("#buttons > button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      playRound(button.id, getBotSelection());
+    });
+  });
+
+  const restartButton = document.querySelector("#restartButton")
+  restartButton.addEventListener("click", () => reset());
+}
+
+function disableRestartButton() {
+  const restartContainer = document.querySelector("#restartContainer");
+  restartContainer.style.display = "none";
+}
+
+function enableRestartButton() {
+  const restartContainer = document.querySelector("#restartContainer");
+  restartContainer.style.display = "flex";
+}
+
+function resetPoints() {
+  const playerPoints = document.querySelector("#playerPoints > .points");
+  playerPoints.textContent = "0";
+  
+  const botPoints = document.querySelector("#botPoints > .points");
+  botPoints.textContent = "0";
+}
+
+function resetResult() {
+  const result = document.querySelector("#result");
+  result.textContent = "Wellcome to the Rock Paper Scissor game";
+}
+
+function reset() {
+  enableButtons();
+  disableRestartButton();
+  resetPoints();
+  resetResult();
+}
+
+function endGame() {
+  disableButtons();
+  enableRestartButton();
+}
+
+disableRestartButton();
+setUpButtons();
